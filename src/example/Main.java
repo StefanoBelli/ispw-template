9b509c922559cce7fcda37c4ad01bfe57af36be5
ssynx <sa.belli@hotmail.it>
2020-11-09 08:53:40 +0100

    Added template

---
+++ src/example/Main.java
@@ -0,0 +1,11 @@
+package example;
+
+import java.util.logging.Logger;
+
+public class Main {
+	private final static Logger logger = Logger.getLogger(Main.class.getName());
+	
+	public static void main(String[] args) {
+		logger.info("Hello world!");
+	}
+}
